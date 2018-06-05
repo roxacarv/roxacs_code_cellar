dd3788573e4aafb1576f6ca4fe4ee4b862491cd7
roxac <xavier.rc791@gmail.com>
2018-06-04 22:00:47 -0300

    BubbleSort for arrays and array-like pointers.

--- Algorithms/BubbleSort/C++/BubbleSort.cpp
+++ Algorithms/BubbleSort/C++/BubbleSort.cpp
@@ -30,13 +30,46 @@ vector<int> BubbleSort(vector<int> vectorToSort)
 		return vectorToSort;
 }
 
+//RECEIVES AN ARRAY BUT RETURNS A POINTER ->
+//WILL RETURN A POINTER NOT AN ARRAY!
+int * BubbleSortForArray(int *arrayToSort, size_t arrayLength)
+{
+	bool change = false;
+	for(int i = 0; i < arrayLength; i++)
+	{
+		if(*(arrayToSort + i) > *(arrayToSort + (i+1)))
+		{
+			int bufferElement = *(arrayToSort + i);
+			*(arrayToSort + i) = *(arrayToSort + (i+1));
+		  *(arrayToSort + (i+1)) = bufferElement;
+			change = true;
+		}
+	}
+	if(change)
+		return BubbleSortForArray(arrayToSort, 20);
+	else
+		return arrayToSort;
+}
+
 int main()
 {
+  //Vector to fill with 20 positions
 	vector<int> newVectorToSort(20);
-	
+  //Array to fill with 20 positions
+	int newArrayToSort[20];
+  //Pointer to fill
+  int * newPointerElements = (int*)malloc(20*sizeof(int));
+  //Generate a random seed
 	srand((unsigned)time(NULL));
-	
-	generate(newVectorToSort.begin(), newVectorToSort.end(), Randomizer);
+  
+  //Sort Vector
+  for(int i = 0; i < 20; i++)
+    newArrayToSort[i] = Randomizer();
+
+  for(int i = 0; i < 20; i++)
+    *(newPointerElements + i) = Randomizer();
+  
+  generate(newVectorToSort.begin(), newVectorToSort.end(), Randomizer);
 
  	printf("Un-sorted Vector: ");
 
@@ -49,5 +82,34 @@ int main()
 	for(int i = 0; i < 20; i++)
     	printf("%d ", finalVector.at(i));
 
+
+  //Sort Array  
+  printf("\n\nUn-sorted Array: ");
+
+  for(int i = 0; i < 20; i++)
+    printf("%d ", newArrayToSort[i]);
+
+  int * SortedArray; //BubbleSortForArray(array, size) returns a pointer
+  //Is necessary to pass the array size. BubbleSortForArray receives an array and its size.
+  SortedArray = BubbleSortForArray(newArrayToSort, 20);
+
+  printf("\n\nSorted Array: ");
+
+  for(int i = 0; i < 20; i++)
+    printf("%d ", SortedArray[i]); //You can access a pointer with Array-style syntax
+
+  //Sort array-like pointer
+  printf("\n\nUn-sorted Array-like Pointer: ");
+
+  for(int i = 0; i < 20; i++)
+    printf("%d ", *(newPointerElements + i));
+
+  int * SortedPointerElements = BubbleSortForArray(newPointerElements, 20);
+
+  printf("\n\nSorted Array-like Pointer: ");
+
+  for(int i = 0; i < 20; i++)
+    printf("%d ", *(SortedPointerElements + i));
+  
 	return 0;
 }
